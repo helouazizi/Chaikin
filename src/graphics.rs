03de3067ae4dcfe33d510e6d6f0d33ba77531f75
helouazizi <ouazizi2code@gmail.com>
2025-09-16 11:55:22 +0100

    ksdfhkjdxfkj

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,1 +1,24 @@
-use minifib::{key, window, windowOption}
+
+
+use minifb::{Key, Window, WindowOptions};
+
+
+
+const WIDTH: usize = 800;
+const HEIGHT: usize = 600;
+
+
+
+pub struct Canvas {
+    window :Window,
+    content : Vec<u32>
+}
+
+
+impl Canvas {
+    pub fn new(name:&str) -> Self{
+        let window = Window::new(name,WIDTH , HEIGHT,WindowOptions::default()).expect("faild to create window");
+        let content = vec![0;WIDTH*HEIGHT];
+        Self{content : content, window : window}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
+mod  graphics;
+mod point;
+mod chaikin;
+
+use graphics::*;
+use point::*;
+use chaikin::*;
+
 fn main() {
-    println!("Hello, hassan!");
+
+    let mut canvas = Canvas::new("Chaikin Curve - ESC to exit");
+
+    let base_points = vec![
+        Point { x: 100.0, y: 300.0 },
+        Point { x: 200.0, y: 100.0 },
+        Point { x: 400.0, y: 150.0 },
+        Point { x: 600.0, y: 300.0 },
+        Point { x: 700.0, y: 500.0 },
+    ];
+
+    // Apply Chaikin's algorithm
+    // let smooth_points = chaikin(&base_points, 3);
+
+    // Main loop
+    while canvas. {
+        canvas.clear();
+        canvas.draw_points(&base_points, 0x00FF00);      
+        // canvas.draw_points(&smooth_points, 0xFFFFFF);    
+        canvas.update();
+    }
 }
 
 
